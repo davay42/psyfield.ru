3ea18a272d1057bc318bee15748345c8655ba64e
DeFUCC <fond@fondfrukt.ru>
2021-03-31 19:55:25 +0300

    emotions

---
+++ cards/adaptive/main.vue
@@ -0,0 +1,33 @@
+<template lang="pug">
+.row
+  .text-4xl.mr-4 {{ emotion?.icon }}
+  .info
+    .text-lg.font-bold.mb-2 {{ emotion?.title }}
+    .q Стимул: 
+      span.font-bold {{ emotion?.event }}
+    .q Когниция: 
+      span.font-bold {{ emotion?.q }}
+    .q Переживание: 
+      span.font-bold {{ emotion?.feeling }}
+    .q Поведение: 
+      span.font-bold {{ emotion?.doing }}
+    .text {{ emotion?.text }}
+</template>
+
+<script setup>
+import { defineProps } from 'vue'
+const props = defineProps({
+  emotion: Object,
+  id: Number,
+});
+
+</script>
+
+<style lang="postcss" scoped>
+.row {
+  @apply my-4 flex;
+}
+.q {
+  @apply mb-2;
+}
+</style>
