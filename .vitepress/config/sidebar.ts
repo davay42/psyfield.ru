a2f0037008a9833076ecee8fb117bd9d975fa864
DeFUCC <fond@fondfrukt.ru>
2021-03-31 13:38:35 +0300

    emotions

--- .vitepress/config/sidebar.ts
+++ .vitepress/config/sidebar.ts
@@ -18,6 +18,7 @@ export const sidebar: DefaultTheme.Config['sidebar'] = {
         { text: 'Позитивная психология', link: '/cards/positive/'},
         { text: 'Нейролингвистическое программирование', link: '/cards/nlp/'},
         { text: 'Спиральная динамика', link: '/cards/spiral/'},
+        { text: 'Адаптационная теория эмоций', link: '/cards/adaptive/'},
         { text: 'Психоалхимия', link: '/cards/alchemy/'},
       ],
     },
@@ -28,6 +29,7 @@ export const sidebar: DefaultTheme.Config['sidebar'] = {
         { text: 'Транзактный анализ', link: '/synopsis/transact/'},
         { text: 'Позитивная психология', link: '/synopsis/positive/'},
         { text: 'Нейролингвистическое программирование', link: '/synopsis/nlp/'},
+        { text: 'Адаптационная теория эмоций', link: '/synopsis/adaptive/'},
       ],
     },
 
