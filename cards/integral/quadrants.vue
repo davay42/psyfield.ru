3ee6cfebbe6701f7c63b5b9ed141b19826fbc4f7
DeFUCC <fond@fondfrukt.ru>
2021-04-05 00:07:04 +0300

    aqal

--- cards/integral/quadrants.vue
+++ cards/integral/quadrants.vue
@@ -2,10 +2,10 @@
 .grid.grid-cols-2.gap-4
   .flex.flex-col.p-4.bg-gray-100(class="dark:bg-warm-gray-800",v-for="q in quadrants", :key="q.content")
     .text-sm {{ q.lr }}–{{ q.tb }}
-    .text-2xl.font-bold.mb-2 {{ q.subj }}
-    .font-bold.mb-2 {{ q.what }}
+    .text-2xl.font-bold.my-4 {{ q.subj }}
+    .font-bold.mb-2.text-lg {{ q.what }}
     .q.mb-2 «{{ q.q }}»
-    .mb-2 {{ q.real }}
+    .mb-2.font-bold {{ q.real }}:
     .text-md {{ q.content }}
 </template>
 
