2c135734fa30aac1e34aedab6285b48bcf5b6521
davay42 <fond@fondfrukt.ru>
2021-04-22 19:15:28 +0300

    childhood periods

--- pages/cards/periods/child.vue
+++ pages/cards/periods/child.vue
@@ -1,21 +1,24 @@
 <template lang="pug">
 .flex.flex-col
-  .flex.flex-col.my-4(v-for="(epoch, i) in $frontmatter.epochs" ) 
-    .flex.items-center.font-bold.mb-2
-      .text-2xl {{ epoch.title }}
+  .flex.flex-col.my-4.shadow-xl.px-8.py-16(v-for="(epoch, i) in $frontmatter.epochs" ) 
+    .flex.items-center.font-bold.mb-2.text-2xl.flex-wrap
+      .flex-1 {{ epoch.title }}
       .flex-auto
-      .text-lg {{ epoch.age }}
-    .flex.flex-col.p-4.my-4(v-for="(period, j) in epoch.periods", :style="levelColor(i * 2 + j, 6)") 
+      .flex-0 {{ epoch.age }}
+    .flex.flex-col.p-4.my-4.shadow(v-for="(period, j) in epoch.periods", :style="levelColor(i * 2 + j, 6)") 
       .text-xl.flex.py-4
         .font-bold {{ period.title }} 
         .flex-auto
         .font-normal ({{ period.age }})
+
       .row
         .title Социальная ситуация развития
         .data {{ period.situation }}
       .row
         .title Ведущая деятельность
+        .text-sm.-mt-2.mb-2 {{ j % 2 ? 'Операционно-техническая сфера' : 'Мотивационно-потребностная сфера' }}
         .data {{ period.action }}
+
       .row
         .title Новообразование
         .data {{ period.new }}
@@ -37,7 +40,6 @@ function levelColor(
   if (reverse) {
     i = n - i - 1
   }
-  console.log(i, n)
   return {
     backgroundColor: `hsla(${i * (360 / n)}, ${s}, ${l}, ${a})`
   }
@@ -47,7 +49,7 @@ function levelColor(
 
 <style lang="postcss" scoped>
 .row {
-  @apply flex flex-wrap my-2;
+  @apply flex flex-col my-2;
 }
 
 .title {
