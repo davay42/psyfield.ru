84f1e528ac0edacfbe7fc5c658aa83db719814db
davay <fond@fondfrukt.ru>
2022-01-05 13:23:06 +0300

    transitions

--- .vitepress/theme/components/page/index.vue
+++ .vitepress/theme/components/page/index.vue
@@ -1,16 +1,14 @@
 <template lang="pug">
 main.main
-  .content(:class="{ 'full-width': frontmatter.fullWidth }")
-    page-parents
-    .text-4xl.font-bold.mb-8.flex.flex-wrap.items-center(v-if="frontmatter.title" :key="frontmatter.title") 
-      .mr-2 {{ frontmatter.title }}
-      .flex-1
-      .mx-2.my-4.text-6xl {{ frontmatter.emoji }}
-    .flex.items-center(v-if="frontmatter.price")
-      .text-xl.font-bold.rounded-xl.text-orange-800.p-2.mr-2(class="dark:text-orange-200") {{ $frontmatter.price }}
-      a.text-xl.font-bold.rounded-xl.bg-orange-300.px-2.py-1(href="/contact") Заказать
-    .font-bold.mt-2.mb-4(v-if="frontmatter.subtitle") {{ frontmatter.subtitle }}
-    content.content(:class="{ cards: !!frontmatter.cards }")
+  transition(name="fade")
+    .content(:class="{ 'full-width': frontmatter.fullWidth }"  :key="route.path")
+      page-parents
+      .text-4xl.font-bold.mb-8.flex.flex-wrap.items-center(v-if="frontmatter.title" :key="frontmatter.title") 
+        .mr-2 {{ frontmatter.title }}
+        .flex-1
+        .mx-2.my-4.text-6xl {{ frontmatter.emoji }}
+      .font-bold.mt-2.mb-4(v-if="frontmatter.subtitle") {{ frontmatter.subtitle }}
+      content.content(:class="{ cards: !!frontmatter.cards }")
   row-list(
     v-if="frontmatter.list", 
     :rows="theme?.pages?.[frontmatter.list]"
@@ -22,6 +20,9 @@ main.main
 <script setup lang="ts">
 import { useData } from 'vitepress'
 const { theme, frontmatter } = useData();
+import { useRoute } from 'vitepress'
+
+const route = useRoute()
 </script>
 
 <style lang="postcss" scoped>
