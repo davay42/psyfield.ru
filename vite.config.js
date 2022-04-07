fc7c535dd3e1e3fca727d0efbc5e597c98bf6e4a
davay <fond@fondfrukt.ru>
2022-04-07 23:56:06 +0300

    no restart

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import Components from 'unplugin-vue-components/vite'
 import Icons from 'unplugin-icons/vite'
 import IconsResolver from 'unplugin-icons/resolver'
 import WindiCSS from 'vite-plugin-windicss'
-import ViteRestart from 'vite-plugin-restart'
 import AutoImport from 'unplugin-auto-import/vite'
 
 import Pages from "vite-plugin-pages";
@@ -51,9 +50,6 @@ export default defineConfig({
         exclude: ['**/examples/**/*'],
         fileExtensions: ['vue', 'ts'],
       },
-    }),
-    ViteRestart({
-      restart: '.vitepress/config/*.*',
     }),
     Pages({
       dirs: [
